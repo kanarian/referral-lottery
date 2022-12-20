41f843ae2c1ff9cb574b3cd7724f724f1fb9272a
kanarian <arian.joyandeh@hotmail.com>
2022-12-20 18:50:56 +0100

    update favicon+remove open link new tab

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,6 @@ const Home: NextPage = () => {
   if (error) {
     return <div>Error</div>;
   }
-  console.log(activeReferrals);
 
   return (
     <>
@@ -60,7 +59,6 @@ const Card = ({
     <Link
       className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
       href={`${link}`}
-      target="_blank"
     >
       <h3 className="text-2xl font-bold">{title} →</h3>
       {/* <div className="text-lg">{description}</div> */}
